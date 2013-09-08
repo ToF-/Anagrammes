fd49fc4cbe50445b93ca43c4c242938064e18d97
ToF- <cthibaut@octo.com>
2013-09-08 15:48:36 +0200

    dictionnaty with one entry can give one anagram

--- public_html/dictionnaire.php
+++ public_html/dictionnaire.php
@@ -1,8 +1,19 @@
 <?php
 
 class Dictionnaire {
+
+	var $entrees;
+
+	function __construct() {
+		$this->entrees = array();
+	}
+
 	function anagrammes($mot) {
-		return array();
+		return $this->entrees;
+	}
+
+	function ajoute($mot) {
+		array_push($this->entrees, $mot);
 	}
 }
 ?>
--- tests/tests_anagrammes.php
+++ tests/tests_anagrammes.php
@@ -6,8 +6,15 @@ class AnagrammesTests extends PHPUnit_Framework_TestCase
     public function test_dictionnaire_vide_alors_aucun_anagramme()
     {
     	$dictionnaire = new Dictionnaire();
-    	$anagrammes = $dictionnaire->anagrammes("TRUC");
-    	$this->assertEquals(array(), $anagrammes);
+    	$this->assertEquals(array(), $dictionnaire->anagrammes("TRUC"));
+    }
+
+    public function test_une_entree_peut_etre_un_anagramme()
+    {
+    	$dictionnaire = new Dictionnaire();
+    	$dictionnaire->ajoute("TURC");
+    	$resultat = array("TURC");
+    	$this->assertEquals($resultat, $dictionnaire->anagrammes("TRUC"));
     }
 }
 ?>
