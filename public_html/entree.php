f22a8b29470f049131c82323a7d4c0edf976247e
ToF- <cthibaut@octo.com>
2013-09-09 10:07:06 +0200

    refactoring Entree for storing key

--- public_html/entree.php
+++ public_html/entree.php
@@ -4,12 +4,17 @@ class Entree {
 
 	function __construct($string) {
 		$this->mot = $string;
+		$this->clef = $this->calcule_clef();
 	}
 	
-	public function clef() {
+	private function calcule_clef() {
 		$a = str_split($this->mot);
 		sort($a);
 		return implode('',$a);
+
+	}
+	public function clef() {
+		return $this->clef;
 	}
 
 	public function mot() {
